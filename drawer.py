05f6d252a28756317d62aa1b5e16686af7d5f8a8
Calvin <calvinmoreiracosta@gmail.com>
2022-11-13 21:19:54 -0300

    Fixed code with help from @FerndWehbe; Now I just have to make the screenshot and text recognition work.

--- drawer.py
+++ drawer.py
@@ -12,24 +12,7 @@ dev_con = win32gui.GetDC(0)
 dcObj = win32ui.CreateDCFromHandle(dev_con)
 hwnd = win32gui.WindowFromPoint((0, 0))
 
-def draw_rect():
-    # with pynput.mouse.Listener(on_move=mlistener.mmove, on_click=mlistener.click) as listener:
-    #     listener.join()
-    #     listener.wait()
-    if mlistener.mouse_pressed >= 0:
-        x0, y0 = win32api.GetCursorPos()
-        print('Agora estou indo para o ciclo while')
-        print(mlistener.mouse_pressed)
-        if mlistener.mouse_pressed >= 0:
-            drawing = True
-        while drawing:
-            # print(win32gui.GetCapture())
-            # rect = win32gui.DrawEdge(dev_con, (x0, y0, x1, y1), win32con.EDGE_BUMP, win32con.BF_RECT)
-            # x1, y1 = win32api.GetCursorPos()
-            print(mlistener.x1,mlistener.y1)
-            rect = win32gui.DrawEdge(dev_con, (x0, y0, mlistener.x1,mlistener.y1), win32con.EDGE_BUMP, win32con.BF_RECT)
-            win32gui.InvalidateRect(hwnd, monitor, True)
-            if win32api.GetKeyState(0x01) < 0:
-                drawing = False
-                # mlistener.mouse_pressed = -127
-
+def draw_rect(x0,y0,x1,y1):
+    print(f"({x0},{y0}),({x1},{y1}")
+    rect = win32gui.DrawEdge(dev_con, (x0, y0,x1,y1), win32con.EDGE_BUMP, win32con.BF_RECT)
+    win32gui.InvalidateRect(hwnd, monitor, True)
--- mlistener.py
+++ mlistener.py
@@ -2,26 +2,33 @@ import pynput
 import win32api
 
 import drawer
-mouse_pressed = 0
+mouse_pressed = False
 x0 = y0 = x1 = y1 = 0
+counter = 0
 def click(x, y,button, pressed):
-    global x0, y0, x1, y1
+    global x0, y0, x1, y1, counter
     global mouse_pressed
     if pressed:
-        mouse_pressed = win32api.GetKeyState(0x01)
+        x0,y0 = win32api.GetCursorPos()
+        mouse_pressed = True
         print(mouse_pressed)
-        drawer.draw_rect()
+        counter += 1
     if not pressed:
-        print('teste')
         # x1 = x
         # y1 = y
-        mouse_pressed = win32api.GetKeyState(0x01)
-        print(mouse_pressed)
-        return False
+        mouse_pressed = False
+
 
 
 def mmove(x,y):
-    global x1, y1, mouse_pressed
-    x1, y1 = win32api.GetCursorPos()
+    global x0,y0,x1, y1, mouse_pressed, counter
+    if mouse_pressed:
+        x1, y1 = win32api.GetCursorPos()
+        drawer.draw_rect(x0, y0, x1, y1)
+
+    if mouse_pressed and counter > 1:
+        print(mouse_pressed)
+        return False
+
     # if win32api.GetKeyState(0x01) < 0:
     #     mouse_pressed = win32api.GetKeyState(0x01)
